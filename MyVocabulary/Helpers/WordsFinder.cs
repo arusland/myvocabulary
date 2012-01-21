6fdaae9989a03c3013486b7ca4956d444746f65a
ruslan.absalyamov@gmail.com <ruslan.absalyamov@gmail.com@5d369b75-d2de-29eb-38dc-3182daefef11>
2012-01-21 19:59:08 +0000

    Added: WordsFinder class.

---
+++ MyVocabulary/Helpers/WordsFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MyVocabulary.Helpers
+{
+    internal class WordsFinder
+    {
+        #region Methods
+        
+        #region Public
+        
+        public IEnumerable<string> Parse(string rawtext)
+        {
+            var mc = Regex.Matches(rawtext, @"[\w\'\-]+");
+
+            return mc.Cast<Match>().Select(p => p.Value.ToLower()).Distinct();
+        }
+        
+        #endregion
+        
+        #endregion
+    }
+}
