d74ec9318f1e89ec32bcb18b1de56371445d9e69
ruslan.absalyamov@gmail.com <ruslan.absalyamov@gmail.com@5d369b75-d2de-29eb-38dc-3182daefef11>
2012-02-10 14:03:29 +0000

    Fixed: double loading words.

--- MyVocabulary/Controls/WordListControl.xaml.cs
+++ MyVocabulary/Controls/WordListControl.xaml.cs
@@ -305,6 +305,7 @@ namespace MyVocabulary.Controls
                 {
                     if (IsModified)
                     {
+                        IsModified = false;
                         LoadItems();
                     }
                 }
